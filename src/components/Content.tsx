4c9e20a7ca851d40a047bb315ab8412196ed13c8
Gabriel <gabriel.devloper@gmail.com>
2021-03-16 19:06:12 -0300

    desafio finalizado

---
+++ src/components/Content.tsx
@@ -0,0 +1,44 @@
+import '../styles/content.scss';
+import { MovieCard } from '../components/MovieCard';
+
+
+interface MovieProps {
+  Title: string;
+  Poster: string;
+  Ratings: Array<{
+    Source: string;
+    Value: string;
+  }>;
+  Runtime: string;
+}
+
+interface GenreResponseProps {
+  id: number;
+  name: 'action' | 'comedy' | 'documentary' | 'drama' | 'horror' | 'family';
+  title: string;
+}
+
+
+interface ContentProps {
+  movies: Array<MovieProps>;
+  selectedGenre: GenreResponseProps
+}
+
+export function Content({ movies, selectedGenre }: ContentProps) {
+  // Complete aqui
+  return (
+      <div className="container">
+        <header>
+          <span className="category">Categoria:<span> {selectedGenre.title}</span></span>
+        </header>
+
+        <main>
+          <div className="movies-list">
+            {movies.map(movie => (
+              <MovieCard title={movie.Title}  poster={movie.Poster} runtime={movie.Runtime} rating={movie.Ratings[0].Value} />
+            ))}
+          </div>
+        </main>
+      </div>
+  )
+}
